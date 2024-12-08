c29845361261023639f73a840af209a0ca9aa947
Ryan Morlok <ryan.morlok@morlok.com>
2024-12-07 21:00:05 -0600

    adding files missed by previous commit

---
+++ database/connection_test.go
@@ -0,0 +1,114 @@
+package database
+
+import (
+	"github.com/google/uuid"
+	"github.com/rmorlok/authproxy/context"
+	"github.com/stretchr/testify/assert"
+	clock "k8s.io/utils/clock/testing"
+	"testing"
+	"time"
+)
+
+func TestConnections(t *testing.T) {
+	t.Run("round trip", func(t *testing.T) {
+		_, db := MustApplyBlankTestDbConfig("connection_round_trip", nil)
+		now := time.Date(1955, time.November, 5, 6, 29, 0, 0, time.UTC)
+		ctx := context.Background().WithClock(clock.NewFakeClock(now))
+
+		u := uuid.New()
+		err := db.CreateConnection(ctx, &Connection{ID: u, State: ConnectionStateCreated})
+		assert.NoError(t, err)
+
+		c, err := db.GetConnection(ctx, u)
+		assert.NoError(t, err)
+		assert.NotNil(t, c)
+		assert.Equal(t, c.ID, u)
+		assert.Equal(t, c.State, ConnectionStateCreated)
+		assert.Equal(t, now, c.CreatedAt)
+		assert.Equal(t, now, c.UpdatedAt)
+	})
+	t.Run("list connections", func(t *testing.T) {
+		_, db := MustApplyBlankTestDbConfig("connection_round_trip", nil)
+		now := time.Date(1955, time.November, 5, 6, 29, 0, 0, time.UTC)
+		c := clock.NewFakeClock(now)
+		ctx := context.Background().WithClock(c)
+
+		var firstUuid, lastUuid uuid.UUID
+		for i := 0; i < 50; i++ {
+			now = now.Add(time.Second)
+			c.SetTime(now)
+
+			u := uuid.New()
+			if i == 0 {
+				firstUuid = u
+			}
+			lastUuid = u
+
+			state := ConnectionStateCreated
+			if i%2 == 1 {
+				state = ConnectionStateReady
+			}
+
+			err := db.CreateConnection(ctx, &Connection{ID: u, State: state})
+			assert.NoError(t, err)
+		}
+
+		t.Run("all connections", func(t *testing.T) {
+			result := db.ListConnectionsBuilder().Limit(10).FetchPage(ctx)
+			assert.NoError(t, result.Error)
+			assert.Len(t, result.Results, 10)
+			assert.Equal(t, result.Results[0].ID, firstUuid)
+			assert.True(t, result.HasMore)
+			assert.NotEmpty(t, result.Cursor)
+
+			total := 10
+			cursor := result.Cursor
+			var last Connection
+
+			for cursor != "" {
+				ex, err := db.ListConnectionsFromCursor(ctx, cursor)
+				assert.NoError(t, err)
+				result = ex.FetchPage(ctx)
+				assert.NoError(t, result.Error)
+				assert.True(t, len(result.Results) > 0)
+
+				last = result.Results[len(result.Results)-1]
+				total += len(result.Results)
+				cursor = result.Cursor
+			}
+
+			assert.Equal(t, 50, total)
+			assert.Equal(t, lastUuid, last.ID)
+		})
+
+		t.Run("filter by state", func(t *testing.T) {
+			total := 0
+
+			q := db.ListConnectionsBuilder().Limit(10).ForConnectionState(ConnectionStateReady)
+			for {
+				result := q.FetchPage(ctx)
+				assert.NoError(t, result.Error)
+				total += len(result.Results)
+				if result.Error != nil || !result.HasMore {
+					break
+				}
+			}
+
+			assert.Equal(t, 25, total)
+		})
+
+		t.Run("reverse order", func(t *testing.T) {
+			var allResults []Connection
+			q := db.ListConnectionsBuilder().Limit(7).OrderBy(ConnectionOrderByCreatedAt, OrderByDesc)
+			err := q.Enumerate(ctx, func(result PageResult[Connection]) (bool, error) {
+				allResults = append(allResults, result.Results...)
+				return true, nil
+			})
+
+			assert.NoError(t, err)
+			assert.Len(t, allResults, 50)
+			assert.Equal(t, lastUuid, allResults[0].ID)
+			assert.Equal(t, firstUuid, allResults[49].ID)
+		})
+	})
+}
---
+++ database/test_db.go
@@ -0,0 +1,69 @@
+package database
+
+import (
+	"fmt"
+	"github.com/google/uuid"
+	"github.com/rmorlok/authproxy/config"
+	"github.com/rmorlok/authproxy/context"
+	"os"
+	"path/filepath"
+	"time"
+)
+
+// MustApplyBlankTestDbConfig applies a test database configuration to the specified config root. The database
+// is guaranteed to be blank and migrated. This method uses a temp file so that the database will be eventually
+// cleaned up after the process exits. Note that the configuration in the root will be modified for the database
+// and populated for the GlobalAESKey if it is not already populated.
+//
+// Parameters:
+// - testName: the name of the test. this can be a blank value but providing it make file names be identifiable by the test that generated them
+// - root: the config to apply the database config to. This may be nil, in which case a new config is created. This method will overwrite the existing config.
+//
+// Returns:
+// - the config with information populated for the database. If a config was passed in, the same value is returned with data populated.
+// - a database instance configured with the specified root. This database can be used directly, or if the root used again, it will connect to the same database instance.
+func MustApplyBlankTestDbConfig(testName string, root *config.Root) (*config.Root, DB) {
+	if testName != "" {
+		testName = testName + "-"
+	}
+
+	if root == nil {
+		root = &config.Root{}
+	}
+
+	tempFilePath := filepath.Join(
+		os.TempDir(),
+		fmt.Sprintf("authproxy-tests/db/%s-%d-%s%s.sqlite3", time.Now().Format("2006-01-02T15-04-05"), os.Getpid(), testName, uuid.New().String()),
+	)
+
+	dirPath := filepath.Dir(tempFilePath)
+	err := os.MkdirAll(dirPath, os.ModePerm)
+	if err != nil {
+		panic(err)
+	}
+
+	_, err = os.Create(tempFilePath)
+	if err != nil {
+		panic(err)
+	}
+
+	root.Database = &config.DatabaseSqlite{
+		Provider: config.DatabaseProviderSqlite,
+		Path:     tempFilePath,
+	}
+	if root.SystemAuth.GlobalAESKey == nil {
+		root.SystemAuth.GlobalAESKey = &config.KeyDataRandomBytes{}
+	}
+
+	db, err := NewConnectionForRoot(root)
+	if err != nil {
+		panic(err)
+	}
+
+	err = db.Migrate(context.Background())
+	if err != nil {
+		panic(err)
+	}
+
+	return root, db
+}
