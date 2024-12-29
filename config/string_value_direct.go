a92161d9617322b53d0342a97f1b9ad247794ea0
Ryan Morlok <ryan.morlok@morlok.com>
2024-12-29 14:18:50 -0600

    more test fixes

--- config/string_value_direct.go
+++ config/string_value_direct.go
@@ -7,7 +7,7 @@ import (
 // StringValueDirect is where the key data is specified directly as bytes. This isn't used for config via file
 // but can be used as way to return data in a config interface that has data already loaded.
 type StringValueDirect struct {
-	Value string `json:"-" yaml:"-"`
+	Value string `json:"value" yaml:"value"`
 }
 
 func (kb *StringValueDirect) HasValue(ctx context.Context) bool {
