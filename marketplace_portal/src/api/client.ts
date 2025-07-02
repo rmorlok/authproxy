f9a9207a905b0b521c401d99076b84f4895f86c2
Ryan Morlok <ryan.morlok@morlok.com>
2025-07-02 11:09:23 -0400

    implemented xsrf support in the marketplace api client

--- marketplace_portal/src/api/client.ts
+++ marketplace_portal/src/api/client.ts
@@ -1,5 +1,13 @@
 import axios from "axios";
 
+// XSRF token storage
+let xsrfToken: string | null = null;
+
+// Function to extract XSRF token from response headers
+const extractXsrfToken = (headers: any): string | null => {
+  return headers['x-xsrf-token'] || headers['X-XSRF-TOKEN'] || null;
+};
+
 export const client = axios.create({
     baseURL: import.meta.env.VITE_PUBLIC_BASE_URL,
     timeout: 200,
@@ -8,3 +16,37 @@ export const client = axios.create({
         'Accept': 'application/json'
     },
 });
+
+// Response interceptor to extract XSRF tokens
+client.interceptors.response.use(
+  (response) => {
+    const token = extractXsrfToken(response.headers);
+    if (token) {
+      xsrfToken = token;
+    }
+    return response;
+  },
+  (error) => {
+    // Also check for XSRF tokens in error responses
+    if (error.response) {
+      const token = extractXsrfToken(error.response.headers);
+      if (token) {
+        xsrfToken = token;
+      }
+    }
+    return Promise.reject(error);
+  }
+);
+
+// Request interceptor to include XSRF tokens
+client.interceptors.request.use(
+  (config) => {
+    if (xsrfToken) {
+      config.headers['X-XSRF-TOKEN'] = xsrfToken;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
