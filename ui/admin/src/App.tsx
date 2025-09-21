304a332aedf76b684c55e0c8ed98ff0cf57d27ea
Ryan Morlok <ryan.morlok@morlok.com>
2025-09-21 16:23:55 -0500

    broke out datagrid compontent for the connections to allow for customization

--- ui/admin/src/App.tsx
+++ ui/admin/src/App.tsx
@@ -1,12 +1,11 @@
 import * as React from 'react';
 import LoadingPage from "./LoadingPage";
 import { RouterProvider } from "react-router-dom";
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 import { selectAuthStatus } from "./store";
 import { router } from './routes';
 
 export default function App() {
-    const dispatch = useDispatch();
     const authStatus = useSelector(selectAuthStatus);
 
     if(authStatus === 'checking' || authStatus === 'redirecting') {
--- ui/admin/src/pages/Connections.tsx
+++ ui/admin/src/pages/Connections.tsx
@@ -2,7 +2,53 @@ import * as React from 'react';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
-import CustomizedDataGrid from '../components/CustomizedDataGrid';
+import {rows} from "../data/gridData";
+import {DataGrid, GridColDef} from "@mui/x-data-grid";
+import Chip from "@mui/material/Chip";
+import {ConnectionState} from "../api";
+
+function renderState(state: ConnectionState) {
+    const colors: Record<ConnectionState, "default" | "success" | "error" | "info" | "warning" | "primary" | "secondary"> = {
+        [ConnectionState.CREATED]: 'default',
+        [ConnectionState.CONNECTED]: 'success',
+        [ConnectionState.FAILED]: 'error',
+        [ConnectionState.DISCONNECTING]: 'info',
+        [ConnectionState.DISCONNECTED]: 'warning'
+    };
+
+    return <Chip label={state} color={colors[state]} size="small" />;
+}
+
+export const columns: GridColDef[] = [
+    { field: 'id',
+        headerName: 'ID',
+        flex: 1.5,
+        minWidth: 200
+    },
+    {
+        field: 'state',
+        headerName: 'State',
+        flex: 0.5,
+        minWidth: 80,
+        renderCell: (params) => renderState(params.value as ConnectionState),
+    },
+    {
+        field: 'created_at',
+        headerName: 'Created At',
+        headerAlign: 'right',
+        align: 'right',
+        flex: 1,
+        minWidth: 80,
+    },
+    {
+        field: 'updated_at',
+        headerName: 'Updated At',
+        headerAlign: 'right',
+        align: 'right',
+        flex: 1,
+        minWidth: 100,
+    },
+];
 
 export default function Connections() {
     return (
@@ -11,7 +57,46 @@ export default function Connections() {
                 Connections
             </Typography>
             <Grid size={{xs: 12, lg: 9}}>
-                <CustomizedDataGrid/>
+                <DataGrid
+                    checkboxSelection
+                    rows={rows}
+                    columns={columns}
+                    getRowClassName={(params) =>
+                        params.indexRelativeToCurrentPage % 2 === 0 ? 'even' : 'odd'
+                    }
+                    initialState={{
+                        pagination: { paginationModel: { pageSize: 20 } },
+                    }}
+                    pageSizeOptions={[10, 20, 50]}
+                    disableColumnResize
+                    density="compact"
+                    slotProps={{
+                        filterPanel: {
+                            filterFormProps: {
+                                logicOperatorInputProps: {
+                                    variant: 'outlined',
+                                    size: 'small',
+                                },
+                                columnInputProps: {
+                                    variant: 'outlined',
+                                    size: 'small',
+                                    sx: { mt: 'auto' },
+                                },
+                                operatorInputProps: {
+                                    variant: 'outlined',
+                                    size: 'small',
+                                    sx: { mt: 'auto' },
+                                },
+                                valueInputProps: {
+                                    InputComponentProps: {
+                                        variant: 'outlined',
+                                        size: 'small',
+                                    },
+                                },
+                            },
+                        },
+                    }}
+                />
             </Grid>
         </Box>
     );
