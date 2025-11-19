c1e4035607442e3701b78666f8ef0e509a1b563d
Ryan Morlok <ryan.morlok@morlok.com>
2025-11-19 07:20:14 -0600

    add humany duration support for days; resolved #13

--- internal/config/common/human_duration.go
+++ internal/config/common/human_duration.go
@@ -3,6 +3,8 @@ package common
 import (
 	"encoding/json"
 	"fmt"
+	"regexp"
+	"strconv"
 	"time"
 
 	"github.com/invopop/jsonschema"
@@ -12,14 +14,67 @@ type HumanDuration struct {
 	time.Duration
 }
 
+const HumanDurationRegex = "^([0-9]+d)?([0-9]+h)?([0-9]+m)?([0-9]+s)?([0-9]+ms)?$"
+
 // JSONSchema customizes the JSON Schema to represent HumanDuration as a string like "60m", "10s", etc.
 func (HumanDuration) JSONSchema() *jsonschema.Schema {
-	return &jsonschema.Schema{Type: "string", Pattern: "^[0-9]+(ns|us|µs|ms|s|m|h)$"}
+	return &jsonschema.Schema{Type: "string", Pattern: HumanDurationRegex}
 }
 
 // MarshalJSON provides custom serialization of the duration to a human-readable string (e.g., "2m").
 func (d HumanDuration) MarshalJSON() ([]byte, error) {
 	return []byte(fmt.Sprintf("\"%s\"", d.String())), nil // `time.Duration.String()` converts duration to "2m", "4h", etc.
+}
+
+func parseHumanDuration(s string) (time.Duration, error) {
+	overallRegex := regexp.MustCompile(HumanDurationRegex)
+	onlyDayRegex := regexp.MustCompile(`^(\d+d)+$`)
+	startsWithDayRegex := regexp.MustCompile(`^(\d+d)(.+)`)
+
+	matched := overallRegex.MatchString(s)
+	if !matched {
+		return time.Duration(0), fmt.Errorf("human duration string does not match required pattern: %s", s)
+	}
+
+	// Manually extract the day piece off the string
+	if onlyDayRegex.MatchString(s) {
+		days, err := strconv.ParseInt(s[:len(s)-1], 10, 64)
+		if err != nil {
+			return time.Duration(0), fmt.Errorf("failed to parse days: %w", err)
+		}
+		return time.Duration(days) * 24 * time.Hour, nil
+	}
+
+	if startsWithDayRegex.MatchString(s) {
+		additionalFromDays := 0 * time.Hour
+		parsedDuration := 0 * time.Second
+
+		parts := startsWithDayRegex.FindStringSubmatch(s)
+		if len(parts) == 3 {
+			// Found a day piece
+			daysStr := parts[1]
+			s = parts[2]
+			days, err := strconv.ParseInt(daysStr[:len(daysStr)-1], 10, 64)
+			if err != nil {
+				return time.Duration(0), fmt.Errorf("failed to parse days: %w", err)
+			}
+			additionalFromDays = time.Duration(days) * 24 * time.Hour
+
+			parsedDuration, err = time.ParseDuration(s)
+			if err != nil {
+				return time.Duration(0), fmt.Errorf("failed to parse duration: %w", err)
+			}
+
+			return parsedDuration + additionalFromDays, nil
+		}
+	}
+
+	// Just a normal duration
+	parsedDuration, err := time.ParseDuration(s)
+	if err != nil {
+		return time.Duration(0), fmt.Errorf("failed to parse duration: %w", err)
+	}
+	return parsedDuration, nil
 }
 
 // UnmarshalJSON parses a human-readable duration string back into `time.Duration`.
@@ -29,7 +84,7 @@ func (d *HumanDuration) UnmarshalJSON(data []byte) error {
 	if len(s) < 2 || s[0] != '"' || s[len(s)-1] != '"' {
 		return fmt.Errorf("invalid duration format: %s", s)
 	}
-	parsedDuration, err := time.ParseDuration(s[1 : len(s)-1])
+	parsedDuration, err := parseHumanDuration(s[1 : len(s)-1])
 	if err != nil {
 		return fmt.Errorf("failed to parse duration: %w", err)
 	}
--- internal/config/common/human_duration_test.go
+++ internal/config/common/human_duration_test.go
@@ -2,13 +2,14 @@ package common
 
 import (
 	"encoding/json"
-	"github.com/stretchr/testify/require"
-	"gopkg.in/yaml.v3"
 	"testing"
 	"time"
+
+	"github.com/stretchr/testify/require"
+	"gopkg.in/yaml.v3"
 )
 
-func TestHumanDurationSerialization(t *testing.T) {
+func TestHumanDuration(t *testing.T) {
 	type TestStruct struct {
 		Duration HumanDuration `json:"duration" yaml:"duration"`
 	}
@@ -52,12 +53,70 @@ func TestHumanDurationSerialization(t *testing.T) {
 		require.Equal(t, duration.Duration, deserialized.Duration)
 	})
 
-	t.Run("Test YAML Serialization", func(t *testing.T) {
+	t.Run("Test YAML Deserialization", func(t *testing.T) {
 		var deserialized TestStruct
 		if err := yaml.Unmarshal([]byte("duration: 6m"), &deserialized); err != nil {
 			t.Fatalf("Failed to deserialize YAML HumanDuration: %v", err)
 		}
 
 		require.Equal(t, time.Minute*6, deserialized.Duration.Duration)
+	})
+
+	t.Run("Test Parse", func(t *testing.T) {
+		tests := []struct {
+			input     string
+			expected  time.Duration
+			expectErr bool
+		}{
+			{
+				input:    "1ms",
+				expected: time.Millisecond,
+			},
+			{
+				input:    "1s",
+				expected: time.Second,
+			},
+			{
+				input:    "1m",
+				expected: time.Minute,
+			},
+			{
+				input:    "1h",
+				expected: time.Hour,
+			},
+			{
+				input:    "1d",
+				expected: 24 * time.Hour,
+			},
+			{
+				input:    "1h30m",
+				expected: time.Hour + 30*time.Minute,
+			},
+			{
+				input:    "1d2h30m",
+				expected: 26*time.Hour + 30*time.Minute,
+			},
+			{
+				input:     "bad",
+				expectErr: true,
+			},
+			{
+				input:     "2d1d",
+				expectErr: true,
+			},
+			{
+				input:     "2s1m",
+				expectErr: true,
+			},
+		}
+		for _, test := range tests {
+			dur, err := parseHumanDuration(test.input)
+			if test.expectErr {
+				require.Error(t, err)
+			} else {
+				require.NoError(t, err)
+				require.Equal(t, test.expected, dur, "Failed to parse duration %s", test.input)
+			}
+		}
 	})
 }
