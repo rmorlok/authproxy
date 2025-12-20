e473d2984c59b58601a036d8f84922b5587e0808
Ryan Morlok <ryan.morlok@morlok.com>
2025-12-20 06:59:04 -0600

    remove unused code

--- internal/jwt/util.go
+++
@@ -1,8 +0,0 @@
-package jwt
-
-import "github.com/golang-jwt/jwt/v5"
-
-// ClaimString converts a singular string into a claims string.
-func ClaimString(s string) jwt.ClaimStrings {
-	return jwt.ClaimStrings{s}
-}
