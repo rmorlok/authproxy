fa8e9f61190d3ee4b0f336e2c6d0bb6e87e435fd
Ryan Morlok <ryan.morlok@morlok.com>
2026-02-16 15:08:36 -0600

    finished work on wrapper to control asynq defaults

--- internal/apasynq/client_interface.go
+++ internal/apasynq/client_interface.go
@@ -14,32 +14,4 @@ type Client interface {
 	Ping() error
 }
 
-type wrappedClient struct {
-	inner    Client
-	defaults []asynq.Option
-}
-
-func (w *wrappedClient) Close() error {
-	return w.inner.Close()
-}
-
-func (w *wrappedClient) Enqueue(task *asynq.Task, opts ...asynq.Option) (*asynq.TaskInfo, error) {
-	return w.inner.Enqueue(task, append(w.defaults, opts...)...)
-}
-
-func (w *wrappedClient) EnqueueContext(ctx context.Context, task *asynq.Task, opts ...asynq.Option) (*asynq.TaskInfo, error) {
-	return w.inner.EnqueueContext(ctx, task, append(w.defaults, opts...)...)
-}
-
-func (w *wrappedClient) Ping() error {
-	return w.inner.Ping()
-}
-
-func WrapClientWithDefaultOptions(c Client, defaultOpts []asynq.Option) Client {
-	return &wrappedClient{
-		inner:    c,
-		defaults: defaultOpts,
-	}
-}
-
 var _ Client = (*asynq.Client)(nil)
--- internal/schema/config/tasks.go
+++ internal/schema/config/tasks.go
@@ -2,5 +2,5 @@ package config
 
 type Tasks struct {
 	// Default retention for tasks unless a value is explicitly set
-	DefaultRetention *HumanDuration
+	DefaultRetention *HumanDuration `json:"default_retention,omitempty" yaml:"default_retention,omitempty"`
 }
