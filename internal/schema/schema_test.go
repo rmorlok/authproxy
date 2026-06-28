c170e69379106b006bd3656bb298765d0a8b7209
Ryan Morlok <ryan.morlok@morlok.com>
2026-06-28 09:31:47 -0500

    Remove schema package reexports

--- internal/schema/schema_test.go
+++ internal/schema/schema_test.go
@@ -3,11 +3,28 @@ package schema
 import (
 	"testing"
 
+	"github.com/rmorlok/authproxy/internal/schema/api"
+	"github.com/rmorlok/authproxy/internal/schema/auth"
+	"github.com/rmorlok/authproxy/internal/schema/common"
+	"github.com/rmorlok/authproxy/internal/schema/config"
+	"github.com/rmorlok/authproxy/internal/schema/resources/connectors"
+	"github.com/rmorlok/authproxy/internal/schema/resources/namespace"
+	"github.com/rmorlok/authproxy/internal/schema/resources/rate_limit"
 	"github.com/stretchr/testify/require"
 )
 
+var allSchemaIDs = []string{
+	api.SchemaIdAPI,
+	auth.SchemaIdAuth,
+	common.SchemaIdCommon,
+	config.SchemaIdConfig,
+	connectors.SchemaIdConnectors,
+	namespace.SchemaIdNamespace,
+	rate_limit.SchemaIdRateLimit,
+}
+
 func Test_AllSchemasCompile(t *testing.T) {
-	for _, schemaId := range allSchemas {
+	for _, schemaId := range allSchemaIDs {
 		_, err := CompileSchema(schemaId)
 		require.NoError(t, err, "schema %s should compile", schemaId)
 	}
