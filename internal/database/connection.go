2dafd2d51b255c6f0bbc3c479fa4d22c2e7edd53
Ryan Morlok <ryan.morlok@morlok.com>
2026-07-17 21:35:48 -0500

    Fix default resource list ordering

--- internal/database/connection.go
+++ internal/database/connection.go
@@ -877,6 +877,11 @@ func (l *listConnectionsFilters) applyRestrictions(ctx context.Context) sq.Selec
 
 	if l.OrderByFieldVal != nil {
 		q = q.OrderBy(fmt.Sprintf("%s %s", string(*l.OrderByFieldVal), l.OrderByVal.String()))
+	} else {
+		// A stable default is required for cursor pagination. In particular, do
+		// not rely on the physical scan order, which can change when indexes are
+		// added or the query planner changes its plan.
+		q = q.OrderBy("created_at ASC", "id ASC")
 	}
 
 	return q
