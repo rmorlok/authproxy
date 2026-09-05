cca1ad210bb2b4855e3d3564ed0f577e94c16f17
Ryan Morlok <ryan.morlok@morlok.com>
2026-09-05 05:15:17 -0500

    test naming fix

--- internal/core/service_rate_limit_scope_test.go
+++ internal/core/service_rate_limit_scope_test.go
@@ -7,35 +7,31 @@ import (
 )
 
 func TestValidateRateLimitScopeTargetNamespace(t *testing.T) {
-	t.Run("connector", func(t *testing.T) {
-		for _, namespace := range []string{
+	for _, namespace := range []string{
+		"root.acme",
+		"root.acme.payments",
+		"root.acme.payments.us",
+	} {
+		err := validateRateLimitScopeTargetNamespace(
+			"connector",
 			"root.acme",
-			"root.acme.payments",
-			"root.acme.payments.us",
-		} {
-			err := validateRateLimitScopeTargetNamespace(
-				"connector",
-				"root.acme",
-				namespace,
-			)
-			require.NoError(t, err)
-		}
-	})
+			namespace,
+		)
+		require.NoError(t, err)
+	}
 
-	t.Run("connection", func(t *testing.T) {
-		for _, namespace := range []string{
-			"root",
-			"root.other",
-			"root.acmes",
-			"root.ac",
-		} {
-			err := validateRateLimitScopeTargetNamespace(
-				"connection",
-				"root.acme",
-				namespace,
-			)
-			require.ErrorIs(t, err, ErrInvalidArgument)
-			require.ErrorContains(t, err, "outside rate-limit namespace")
-		}
-	})
+	for _, namespace := range []string{
+		"root",
+		"root.other",
+		"root.acmes",
+		"root.ac",
+	} {
+		err := validateRateLimitScopeTargetNamespace(
+			"connection",
+			"root.acme",
+			namespace,
+		)
+		require.ErrorIs(t, err, ErrInvalidArgument)
+		require.ErrorContains(t, err, "outside rate-limit namespace")
+	}
 }
